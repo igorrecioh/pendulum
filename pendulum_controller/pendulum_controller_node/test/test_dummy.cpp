89978ffedcfda93ff555a7c032849e6c549aa1d7
Igor Recio <igor.recio.h@gmail.com>
2020-08-05 00:10:22 +0200

    First iteration test

--- pendulum_controller/pendulum_controller_node/test/test_dummy.cpp
+++ pendulum_controller/pendulum_controller_node/test/test_dummy.cpp
@@ -13,58 +13,19 @@
 // limitations under the License.
 
 #include <gtest/gtest.h>
+#include <memory>
+#include <typeinfo>
 #include "rclcpp/rclcpp.hpp"
-#include "pendulum_driver/pendulum_driver_node.hpp"
 #include "pendulum_controller_node/pendulum_controller_node.hpp"
-#include "pendulum_controller_node/pendulum_controller.hpp"
-#include "pendulum_controllers/full_state_feedback_controller.hpp"
-#include "pendulum_simulation/pendulum_simulation.hpp"
 
-static const size_t DEFAULT_DEADLINE_PERIOD_US = 2000;
-static const size_t DEFAULT_CONTROLLER_UPDATE_PERIOD_US = 1000;
-static const size_t DEFAULT_PHYSICS_UPDATE_PERIOD_US = 1000;
+using rclcpp::executors::MultiThreadedExecutor;
 
 TEST(DummyTest, not_equal) {
-  std::chrono::microseconds deadline_duration(DEFAULT_DEADLINE_PERIOD_US);
-
-  // controller options
-  std::chrono::microseconds controller_update_period(DEFAULT_CONTROLLER_UPDATE_PERIOD_US);
-  std::vector<double> feedback_matrix = {-10.0000, -51.5393, 356.8637, 154.4146};
-
-  // driver options
-  std::chrono::microseconds physics_update_period(DEFAULT_PHYSICS_UPDATE_PERIOD_US);
-
-  // set QoS deadline period
-  rclcpp::QoS qos_deadline_profile(10);
-  qos_deadline_profile.deadline(deadline_duration);
-
-  // Create a controller
-  std::unique_ptr<pendulum::PendulumController> controller = std::make_unique<
-    pendulum::FullStateFeedbackController>(feedback_matrix);
-
-  // Create pendulum simulation
-  std::unique_ptr<pendulum::PendulumDriverInterface> sim =
-    std::make_unique<pendulum::PendulumSimulation>(physics_update_period);
-
-  // Create pendulum controller node
-  pendulum::PendulumControllerOptions controller_options;
-  controller_options.node_name = "pendulum_controller";
-  controller_options.command_publish_period = controller_update_period;
-  controller_options.status_qos_profile = qos_deadline_profile;
-  controller_options.command_qos_profile = qos_deadline_profile;
-  controller_options.setpoint_qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-
-  // Create pendulum driver node
-  pendulum::PendulumDriverOptions driver_options;
-  driver_options.node_name = "pendulum_driver";
-  // set sensor publishing period equal to simulation physics update period
-  driver_options.status_publish_period = physics_update_period;
-  driver_options.status_qos_profile = qos_deadline_profile;
-
-  auto pendulum_driver = std::make_shared<pendulum::PendulumDriverNode>(
-    std::move(sim),
-    driver_options,
-    rclcpp::NodeOptions().use_intra_process_comms(true));
+  MultiThreadedExecutor executor;
+  auto pendulum_driver = std::make_shared<
+    pendulum::PendulumControllerNode>(rclcpp::NodeOptions());
+  std::cout << typeid(pendulum_driver).name() << std::endl;
+  executor.add_node(pendulum_driver->get_node_base_interface());
 
   EXPECT_EQ(1, 1);
 }
